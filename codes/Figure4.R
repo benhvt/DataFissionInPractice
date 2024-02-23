e48c9de4741317d9b38d225f702641b5e37b62f3
Benjamin Hivert <ben.hivert@gmail.com>
2024-02-23 10:47:32 +0100

    Update on Figure 4

--- codes/Figure4.R
+++ codes/Figure4.R
@@ -13,55 +13,9 @@ theme_set(theme_bw())
 source("utils.R")
 
 #-- Parameters 
-n <- 500
+n <- 100
 true_class <- rep(1:2, each = n/2)
 nsimu <- 1000
-
-#-- Simulations with the true number of classes 
-sim_res <- pblapply(1:nsimu, function(ns){
-  set.seed(310123*ns)
-  x1 <- rnbinom(n=n/2, prob = 0.5, size = 5)
-  x2 <- rnbinom(n=n/2, prob = 0.4, size = 40)
-  y <- rnbinom(n=n, prob = .5, size = 5)
-  X <- cbind(c(x1,x2), 
-             y)
-
-  #--- Data thinning with the global parameter
-  #-- Overdisperion estimation 
-  overdisp_global <- sapply(1:ncol(X), function(p){
-    npreg::theta.mle(X[,p], mu = mean(X[,p]))
-  })
-  
-  res <- countsplit::countsplit(X, overdisps = overdisp_global)
-  clust <- km_fun(as.matrix(res[[1]]), K=2)
-  ari <- mclust::adjustedRandIndex(clust, true_class)
-  
-  #--- Data thinning with the true intra-class parameter 
-  #-- Overdisperion estimation 
-  
-  overdisp_C1 <- sapply(1:ncol(X), function(p){
-    npreg::theta.mle(X[true_class==1,p], mu = mean(X[true_class==1,p]))
-  })
-  
-  overdisp_C2 <- sapply(1:ncol(X), function(p){
-    npreg::theta.mle(X[true_class==2,p], mu = mean(X[true_class==2,p]))
-  })
-  overdisp_intra <- list(overdisp_C1, overdisp_C2)
-  res_temp <- lapply(1:2, function(cl){
-    res_datathin <- countsplit::countsplit(X[true_class==cl, ], overdisps = overdisp_intra[[cl]])
-  })
-  
-  Xtrain <- rbind(as.matrix(res_temp[[1]][[1]]),
-                  as.matrix(res_temp[[2]][[1]]))
-  Xtest <- rbind(as.matrix(res_temp[[1]][[2]]),
-                 as.matrix(res_temp[[2]][[2]]))
-  
-  clust_intra <- km_fun(Xtrain, K=2)
-  ari_intra <- mclust::adjustedRandIndex(clust_intra, true_class)
-  return(data.frame(ARI = c(ari, ari_intra),
-                    Overdispersion = c("Global", "Intra-comp")))
-}, cl = 6)
-
 
 #-- Simulations with the wrong number of classes 
 sim_res_wrong <- pblapply(1:nsimu, function(ns){
@@ -73,7 +27,6 @@ sim_res_wrong <- pblapply(1:nsimu, function(ns){
              y)
   
   #--- Data thinning with the global parameter
-  #-- Overdisperion estimation 
   overdisp_global <- sapply(1:ncol(X), function(p){
     npreg::theta.mle(X[,p], mu = mean(X[,p]))
   })
@@ -85,8 +38,6 @@ sim_res_wrong <- pblapply(1:nsimu, function(ns){
                           as.matrix(res[[2]])[clust == clToTest[2],1])$p.value
   
   #--- Data thinning with the true intra-class parameter 
-  #-- Overdisperion estimation 
-  
   overdisp_C1 <- sapply(1:ncol(X), function(p){
     npreg::theta.mle(X[true_class==1,p], mu = mean(X[true_class==1,p]))
   })
@@ -108,8 +59,42 @@ sim_res_wrong <- pblapply(1:nsimu, function(ns){
   clToTest_intra <- order_cluster(Xtrain[,1], clust_intra)
   res_test_intra <- wilcox.test(Xtest[clust_intra==clToTest_intra[1],1],
                           Xtest[clust_intra==clToTest_intra[2],1])$p.value  
-  return(data.frame(pval = c(res_test, res_test_intra),
-                    Overdispersion = c("Global", "Intra-comp")))
+  
+  #--- Data thinning with the wrong intra-cluster parameter 
+  cl_ref <- km_fun(X, K=3)
+  
+  overdisp_C1_hat <- sapply(1:ncol(X), function(p){
+    npreg::theta.mle(X[cl_ref==1,p], mu = mean(X[cl_ref==1,p]))
+  })
+  
+  overdisp_C2_hat <- sapply(1:ncol(X), function(p){
+    npreg::theta.mle(X[cl_ref==2,p], mu = mean(X[cl_ref==2,p]))
+  })
+  
+  overdisp_C3_hat <- sapply(1:ncol(X), function(p){
+    npreg::theta.mle(X[cl_ref==3,p], mu = mean(X[cl_ref==3,p]))
+  })
+  
+  overdisp_intra_hat <- list(overdisp_C1_hat, overdisp_C2_hat, overdisp_C3_hat)
+  res_temp_hat <- lapply(1:3, function(cl){
+    res_datathin <- countsplit::countsplit(X[cl_ref==cl, ], overdisps = overdisp_intra_hat[[cl]])
+  })
+  
+  Xtrain_hat <- rbind(as.matrix(res_temp_hat[[1]][[1]]),
+                  as.matrix(res_temp_hat[[2]][[1]]),
+                  as.matrix(res_temp_hat[[3]][[1]]))
+  Xtest_hat <- rbind(as.matrix(res_temp_hat[[1]][[2]]),
+                 as.matrix(res_temp_hat[[2]][[2]]),
+                 as.matrix(res_temp_hat[[3]][[2]]))
+  
+  
+  clust_intra_hat <- km_fun(Xtrain_hat, K=3)
+  clToTest_intra_hat <- order_cluster(Xtrain_hat[,1], clust_intra_hat)
+  res_test_intra_hat <- wilcox.test(Xtest_hat[clust_intra_hat==clToTest_intra_hat[1],1],
+                                Xtest_hat[clust_intra_hat==clToTest_intra_hat[2],1])$p.value  
+  
+  return(data.frame(pval = c(res_test, res_test_intra, res_test_intra_hat),
+                    Overdispersion = c("Global", "Intra-comp", "Intra-cluster")))
 }, cl = 6)
 
 
@@ -146,12 +131,14 @@ plt1 <- ggplot(X) + aes(x=X1, y=X2) +
 plt2 <- do.call(rbind.data.frame, sim_res_wrong) %>% 
   ggplot() + 
   geom_abline(slope=1, intercept=0, col="red", size = 1.2, alpha = .7) + xlab("Theoretical Quantiles") + 
-  stat_qq(aes(sample = pval, colour = factor(Overdispersion)),
+  stat_qq(aes(sample = pval, colour = factor(Overdispersion,
+                                             levels = c('Intra-comp', 'Intra-cluster', 'Global'))),
               distribution = qunif, size = 3) +
   scale_colour_manual(name = "Overdispersion", 
-                      values = results_col[c(3,2)],
-                      labels=c("Global",
-                               "True intra-classe")) +
+                      values = results_col,
+                      labels=c(TeX(r'($\theta_{g}$)'),
+                               TeX(r'($\hat{\theta}_{\hat{g}}$)'),
+                               TeX(r'($\hat{\theta}$)'))) +
   ylab("Empirical Quantiles") + 
   xlim(c(0, 1)) + ylim(c(0, 1)) + theme_classic() 
 plt2
