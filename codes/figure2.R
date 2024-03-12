dd17d46b6df46b5ced76aa6ea387458a642d54de
Benjamin Hivert <ben.hivert@gmail.com>
2024-03-12 15:13:43 +0100

    Change in Figure 2

--- codes/figure2.R
+++ codes/figure2.R
@@ -44,28 +44,25 @@ tau <- .4
 
 sim_res <- sim_fun_sigma(n=n, sigma = sigma, sigma_hat = sigma_grid, tau = tau, nsim = nsimu) 
 
-plot_sigma <- data.frame(EmpTypeI = unlist(sim_res),
-           sigma = rep(sigma, each = length(sigma_grid)),
-           sigma_hat = rep(sigma_grid, length(sigma))) %>%
+df_res_sigma <- data.frame(EmpTypeI = unlist(sim_res),
+                           sigma = rep(sigma, each = length(sigma_grid)),
+                           sigma_hat = rep(sigma_grid, length(sigma))) %>%
   mutate(TheTypeI = compute_typeI(n = n, tau = tau, sigma = sigma, sigma_hat = sigma_hat, alpha = .05)) %>%
   mutate(sigma_name = paste0("sigma^2==", sigma^2)) %>%
   mutate(Ratio = (sigma^2-sigma_hat^2)/sigma^2) %>%
-  ggplot() + aes(x=Ratio, y = EmpTypeI, colour = sigma_name) +
-  geom_point(shape = 2, size = 4) +  
-  scale_colour_manual(name = 'Empirical Type I error rate',
+  reshape2::melt(id.vars = c("Ratio", "sigma_name", "sigma", "sigma_hat"), measure_vars = c("TheTypeI", "EmpTypeI"))
+
+plot_sigma <-  ggplot(df_res_sigma) + aes(x=Ratio, y = value, colour = sigma_name) +
+  geom_point(data = subset(df_res_sigma, variable == "EmpTypeI"), aes(shape = "Empirical"), size = 4) +
+  scale_shape_manual(name = "Empirical", values = 2, labels = '', guide = "legend") +
+  geom_line(data = subset(df_res_sigma, variable == "TheTypeI"), aes(group = sigma_name, linetype = "Theoritical"), size = 1) +
+  scale_linetype_manual(name = "Theoritical", values = 1, labels = "", guide = "legend") +
+  scale_colour_manual(name = '',
                       values = c("#93B5C6", "#DBC2CF", "#998bc0", "#BD4F6C"),
                       labels = c(TeX(r'($\sigma^2 = 0.01$)'),
                                           TeX(r'($\sigma^2 = 0.25$)'),
                                           TeX(r'($\sigma^2 = 1$)'),
                                           TeX(r'($\sigma^2 = 4$)'))) +
-  ggnewscale::new_scale_colour() +
-  geom_line(aes(x=Ratio, y = TheTypeI, colour = sigma_name), linewidth = 1.5, alpha = .8) +
-  scale_colour_manual(name = "Theoritical Type I error rate",
-                      values = c("#677f8b", "#998891", "#6b6186", "#84374c"),
-                      labels = c(TeX(r'($\sigma^2 = 0.01$)'),
-                                 TeX(r'($\sigma^2 = 0.25$)'),
-                                 TeX(r'($\sigma^2 = 1$)'),
-                                 TeX(r'($\sigma^2 = 4$)'))) +
   xlab(TeX(r'($(\sigma^2 - \hat{\sigma^2})/\sigma^2$)')) +
   xlim(c(-5,2)) +
   ylab("Type I error rate") +
@@ -76,6 +73,11 @@ plot_sigma <- data.frame(EmpTypeI = unlist(sim_res),
              size = 1.2) +
   scale_colour_manual(name = "",
                       values = "#6C0E23") +
+  guides(
+    shape = guide_legend(order = 1),
+    linetype = guide_legend(order = 2),
+    color = guide_legend(order = 3)
+  ) +
   NULL
 
 
@@ -95,22 +97,22 @@ tau <- .4
 
 sim_res_n <- sim_fun_n(n_grid=n, sigma = sigma, sigma_hat = sigma_grid, tau = tau, nsim = nsimu) 
 
-#-- Plot 
-plot_n <- data.frame(EmpTypeI = unlist(sim_res_n),
-           sigma = sigma,
-           SampSize = rep(n, each = length(sigma_grid)),
-           sigma_hat = rep(sigma_grid, length(n))) %>%
+df_res_n <-  data.frame(EmpTypeI = unlist(sim_res_n),
+                        sigma = sigma,
+                        SampSize = rep(n, each = length(sigma_grid)),
+                        sigma_hat = rep(sigma_grid, length(n))) %>%
   mutate(TheTypeI = compute_typeI(n = SampSize, tau = tau, sigma = sigma, sigma_hat = sigma_hat, alpha = .05)) %>%
   mutate(sigma_name = paste0("sigma^2==", sigma^2)) %>%
   mutate(Ratio = (sigma^2-sigma_hat^2)/sigma^2) %>%
-  ggplot() + aes(x=Ratio, y = EmpTypeI, colour = as.factor(SampSize)) +
-  geom_point(shape = 2, size = 3) +  
-  scale_colour_manual(name = "Empirical Type I error rate", 
-                      values = colorRampPalette(c("#008154", "#0092a4", "#2a2956"))(length(n)),
-                      labels = paste0("n=", n)) +
-  ggnewscale::new_scale_colour() +
-  geom_line(aes(x=Ratio, y = TheTypeI, colour = as.factor(SampSize)), linewidth = 1.5) +
-  scale_colour_manual(name = "Theoritical Type I error rate", 
+  reshape2::melt(id.vars = c("Ratio", "sigma_name", "SampSize", "sigma", "sigma_hat"), measure_vars = c("TheTypeI", "EmpTypeI"))
+
+#-- Plot 
+plot_n <- ggplot(df_res_n) + aes(x=Ratio, y = value, colour = as.factor(SampSize)) +
+  geom_point(data = subset(df_res_n, variable == "EmpTypeI"), aes(shape = "Empirical"), size = 4) +
+  scale_shape_manual(name = "Empirical", values = 2, labels = '', guide = "legend") +
+  geom_line(data = subset(df_res_n, variable == "TheTypeI"), aes(group = SampSize, linetype = "Theoritical"), size = 1) +
+  scale_linetype_manual(name = "Theoritical", values = 1, labels = "", guide = "legend") +
+  scale_colour_manual(name = "", 
                       values = colorRampPalette(c("#008154", "#0092a4", "#2a2956"))(length(n)),
                       labels = paste0("n=", n)) +
   xlab(TeX(r'($(\sigma^2 - \hat{\sigma^2})/\sigma^2$)')) +
@@ -122,19 +124,24 @@ plot_n <- data.frame(EmpTypeI = unlist(sim_res_n),
              size = 1.2) +
   scale_colour_manual(name = "",
                       values = "#6C0E23") +
+  guides(
+    shape = guide_legend(order = 1),
+    linetype = guide_legend(order = 2),
+    color = guide_legend(order = 3)
+  ) +
   NULL
 
-plot_sigma + plot_n + plot_annotation(tag_levels = "A") &
-  theme(axis.title = element_text(size = 16), 
-        axis.text = element_text(size = 16),
-        # legend.position = "bottom",
+(plot_sigma + plot_n)  +
+  plot_annotation(tag_levels = "A") &
+  theme(axis.title = element_text(size = 24), 
+        axis.text = element_text(size = 18),
         legend.text = element_text(size = 16),
-        legend.title = element_text(size = 16),
-        strip.text = element_text(size = 16),
-        plot.tag = element_text(face = "bold", size = 16))
+        legend.title = element_text(size = 18),
+        plot.tag = element_text(face = "bold", size = 24),
+        legend.spacing = unit(0.01, "cm"))
 ggsave(filename = "figures/figure2.pdf",
-       width = 400,
-       height = 120,
+       width = 350, 
+       height = 120, 
        units = "mm",
        dpi = 600)
-          
+             
